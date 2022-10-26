995fdc4c252383f1d0a3b0b7942d6aefefe9bbbc
Hanna Kraska <hanna.kraska11@gmail.com>
2022-10-26 21:58:10 +0200

    add socket emit for searching songs

--- index.js
+++ index.js
@@ -21,7 +21,48 @@ const io = require("socket.io")(server, {
   allowEIO3: true,
 });
 
-const trackListData = ["cry", "Not Rickroll"];
+const searchTrackData = [
+  { title: "Cry For Me", artist: "Twice", album: "Cry For Me" },
+  {
+    title: "Dance The Night Away",
+    artist: "Twice",
+    album: "Dance The Night Away",
+  },
+  { title: "Feel Special", artist: "Twice", album: "Feel Special" },
+  { title: "Fancy", artist: "Twice", album: "Fancy" },
+  { title: "Heart Shaker", artist: "Twice", album: "Heart Shaker" },
+  { title: "Likey", artist: "Twice", album: "Likey" },
+  { title: "Like Ooh-Ahh", artist: "Twice", album: "Like Ooh-Ahh" },
+  { title: "Signal", artist: "Twice", album: "Signal" },
+  { title: "TT", artist: "Twice", album: "TT" },
+  { title: "What Is Love", artist: "Twice", album: "What Is Love" },
+  { title: "Yes Or Yes", artist: "Twice", album: "Yes Or Yes" },
+  { title: "Cheer Up", artist: "Twice", album: "Cheer Up" },
+  { title: "Knock Knock", artist: "Twice", album: "Knock Knock" },
+  { title: "Merry & Happy", artist: "Twice", album: "Merry & Happy" },
+  { title: "One More Time", artist: "Twice", album: "One More Time" },
+  { title: "OOH-AHH하게", artist: "Twice", album: "OOH-AHH하게" },
+  { title: "SIGNAL", artist: "Twice", album: "SIGNAL" },
+  { title: "TT", artist: "Twice", album: "TT" },
+  { title: "Yes Or Yes", artist: "Twice", album: "Yes Or Yes" },
+  { title: "DDU-DU-DDU-DU", artist: "Blackpink", album: "Square Uo" },
+  { title: "Kill This Love", artist: "Blackpink", album: "Kill This Love" },
+  {
+    title: "How You Like That",
+    artist: "Blackpink",
+    album: "How You Like That",
+  },
+  { title: "Ice Cream", artist: "Blackpink", album: "Ice Cream" },
+  { title: "Icy", artist: "ITZY", album: "Icy" },
+  { title: "ICY", artist: "ITZY", album: "ICY" },
+  { title: "Dalla Dalla", artist: "ITZY", album: "Dalla Dalla" },
+  { title: "Not Shy", artist: "ITZY", album: "Not Shy" },
+  { title: "Wannabe", artist: "ITZY", album: "Wannabe" },
+  { title: "HIP", artist: "Mamamoo", album: "HIP" },
+  { title: "Egotistic", artist: "Mamamoo", album: "Egotistic" },
+  { title: "Starry Night", artist: "Mamamoo", album: "Starry Night" },
+  { title: "Wind Flower", artist: "Mamamoo", album: "Wind Flower" },
+];
 
 io.engine.on("connection_error", (err) => {
   console.log(err);
@@ -30,15 +71,23 @@ io.engine.on("connection_error", (err) => {
 io.on("connection", function (socket) {
   console.log("Established socket connection");
   console.log("User connected: " + socket.id);
-  socket.emit("track-list", trackListData);
+  socket.on("get-track-list", () => {
+    socket.emit("send-track-list", searchTrackData);
+  });
+
+  socket.on("search-for-track", (title) => {
+    const result = searchTrackData.find((track) => {
+      return track.title.toLowerCase().includes(title.toLowerCase());
+    });
+    console.log(result);
+    console.log(typeof result);
+    socket.broadcast.emit("send-track-info", result);
+  });
 
   socket.on("send_message_to_server", (data) => {
     console.log(data);
     socket.broadcast.emit("broadcast_message", data);
   });
-
-  socket.emit("track-list", trackListData);
-
   socket.on("play_music", (data) => {
     console.log("halkoooo");
     console.log(data.filename);
@@ -51,13 +100,9 @@ io.on("connection", function (socket) {
     // ss(socket).emit('play-song', stream, { name: filename });
 
     const trackArray = fs.readFileSync(filename).buffer;
-    console.log(filename);
-    console.log();
-    console.log(typeof trackArray);
-    console.log(trackArray instanceof ArrayBuffer);
 
     socket.emit(
-      "play-song",
+      "send-song",
       trackArray,
       { name: filename },
       { type: "audio/mpeg" }
